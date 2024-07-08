1649ce495ba24504a92f26d0bbc2145e3ed8f0c5
mariusdgm <dragomir10m@yahoo.com>
2024-07-08 23:31:52 +0300

    added util function

--- dynamic_programming/utils.py
+++ dynamic_programming/utils.py
@@ -18,3 +18,21 @@ def extract_V_from_Q_for_stochastic_policy(Q, policy, states):
     for state in states:
         V[state] = sum(policy[state][a] * Q[state][a] for a in policy[state])
     return V
+
+
+def derive_policy_from_q_table(q_table, states, actions):
+    """
+    Derive the optimal policy from the Q-table.
+
+    Args:
+    - q_table (dict): A dictionary where keys are states and values are dictionaries of action-value pairs.
+    - states (list): A list of all possible states.
+    - actions (list): A list of all possible actions.
+
+    Returns:
+    - policy (dict): A dictionary where keys are states and values are the optimal actions.
+    """
+    policy = {}
+    for state in states:
+        policy[state] = max(actions, key=lambda a: q_table[state][a])
+    return policy
