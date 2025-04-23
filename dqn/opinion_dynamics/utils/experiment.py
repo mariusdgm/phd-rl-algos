98ef6b0fc66164a498343026117a84184ed9f947
mariusdgm <dragomir10m@yahoo.com>
2025-04-23 22:50:02 +0300

    moved code, made code to inspect all seeds in a ran env

--- dqn/opinion_dynamics/utils/experiment.py
+++ dqn/opinion_dynamics/utils/experiment.py
@@ -2,8 +2,11 @@ import os
 
 import random
 import torch
-import numpy as np
+import yaml
 import datetime
+import collections
+import numpy as np
+import pandas as pd
 
 from pathlib import Path
 from typing import Dict
@@ -68,6 +71,115 @@ def create_path_to_experiment_folder(
 
     return exp_folder_path
 
+
+def process_experiment(root_dir):
+    rows = []
+
+    for name in os.listdir(root_dir):
+        experiment_path = os.path.join(root_dir, name)
+        if os.path.isdir(experiment_path):
+            for seed_name in os.listdir(experiment_path):
+                seed_path = os.path.join(experiment_path, seed_name)
+                if os.path.isdir(seed_path):
+                    row_data = process_subexperiment(
+                        seed_path, os.path.basename(root_dir)
+                    )
+                    for data in row_data:
+                        data["seed"] = seed_name
+                        data["experiment_name"] = name
+                        data["sub_experiment_path"] = seed_path
+                    rows.extend(row_data)  
+
+    # Create a DataFrame from the rows
+    df = pd.DataFrame(rows)
+    return df
+
+def process_subexperiment(seed_folder_path, experiment_name):
+    cfg_data = read_config(os.path.join(seed_folder_path, "cfg.yaml"), experiment_name)
+    cfg_data["sub_experiment_path"] = seed_folder_path 
+    train_stats_file = find_train_stats_file(seed_folder_path)
+    if train_stats_file:
+        experiment_results = process_training_stats(
+            train_stats_file, cfg_data
+        )
+        return experiment_results
+    else:
+        return []  # Return an empty list if no train stats file is found
+
+
+def read_config(cfg_path, experiment_name):
+    with open(cfg_path, "r") as file:
+        config = yaml.safe_load(file)
+        full_title = config.get("full_title", "")
+        variable_part = remove_experiment_name(full_title, experiment_name)
+        return parse_config_variables(variable_part)
+
+
+def remove_experiment_name(full_title, experiment_name):
+    to_remove = experiment_name + "_"
+    return (
+        full_title[len(to_remove) :].strip()
+        if full_title.startswith(to_remove)
+        else full_title
+    )
+
+
+def parse_config_variables(variable_str):
+    variables = {}
+    for part in variable_str.split(";"):
+        if "=" in part:
+            key, value = part.split("=", 1)
+            key = f"sub_exp_cfg_{key.strip()}"  # Add prefix
+            variables[key] = value.strip()
+    return variables
+
+
+def find_train_stats_file(folder_path):
+    for file in os.listdir(folder_path):
+        if file.endswith("_train_stats"):
+            return os.path.join(folder_path, file)
+    return None
+
+
+def process_training_stats(train_stats_file, cfg_data):
+    checkpoint = torch.load(train_stats_file)
+
+    training_stats = checkpoint.get("training_stats", [])
+    validation_stats = checkpoint.get("validation_stats", [])
+    redo_stats = checkpoint.get("redo_scores", [])
+
+    stats_records = process_stats(training_stats, cfg_data, "training") + process_stats(
+        validation_stats, cfg_data, "validation"
+    )
+
+    # Combine stats records with redo scores
+    combined_records = []
+    for record in stats_records:
+        combined_record = record.copy()  # Copy the stats record
+        combined_records.append(combined_record)
+
+    return combined_records
+
+
+def process_stats(stats, cfg_data, stats_type):
+    records = []
+    for epoch_stats in stats:
+        record = {"epoch_type": stats_type}
+        record.update(flatten(epoch_stats))  # Flatten the epoch_stats if it's nested
+        record.update(cfg_data)  # Add configuration data
+        records.append(record)
+    return records
+
+
+def flatten(d, parent_key="", sep="_"):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.abc.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
 
 def create_adjacency_matrix_from_links(num_nodes, links):
     adjacency_matrix = np.zeros((num_nodes, num_nodes), dtype=int)
