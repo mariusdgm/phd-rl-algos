222cc0885a71b7edf799d5e1b2525fa4f32e47d5
mariusdgm <dragomir10m@yahoo.com>
2025-04-24 23:08:56 +0300

    updated logging handling

--- dqn/opinion_dynamics/experiments/README.md
+++ dqn/opinion_dynamics/experiments/README.md
@@ -9,3 +9,6 @@ liftoff-prepare configs --do --runs-no 2
 ```
 liftoff training_opinion.py .\results\2025Apr21-222011_configs --procs-no 5
 ```
+
+beta, nu e u_max ca pana acum, ci limitat la n noduri influente
+change reward to use original action
--- dqn/opinion_dynamics/utils/my_logging.py
+++ dqn/opinion_dynamics/utils/my_logging.py
@@ -9,7 +9,7 @@ def setup_logger(name, log_file=None, level=logging.INFO):
     """
     Create and return a customized logger.
 
-    Args: 
+    Args:
         env_name: identifier string to be used in the name of the file (if the log file is created).
         folder_path: folder where to save the log file. If None, no folder is created.
         identifier_string: an identifier string that will appear in each logged message.
@@ -20,25 +20,26 @@ def setup_logger(name, log_file=None, level=logging.INFO):
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
-    formatter = logging.Formatter(
+    if not logger.handlers:
+        formatter = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s"
         )
-    
-    # Create a console handler
-    console_handler = logging.StreamHandler(stream=sys.stdout)
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
 
-    # If a log file path is provided, set up file handler
-    if log_file is not None:
-        file_handler = RotatingFileHandler(
-            log_file, maxBytes=1024 * 1024 * 5, backupCount=3
-        )  # 5MB per file, with 3 backups
-       
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+        # Create a console handler
+        console_handler = logging.StreamHandler(stream=sys.stdout)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
 
-    return logger
+        # If a log file path is provided, set up file handler
+        if log_file is not None:
+            file_handler = RotatingFileHandler(
+                log_file, maxBytes=1024 * 1024 * 5, backupCount=3
+            )  # 5MB per file, with 3 backups
+
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+
+        return logger
 
 
 def cleanup_file_handlers(experiment_logger=None):
@@ -53,10 +54,9 @@ def cleanup_file_handlers(experiment_logger=None):
         for handler in experiment_logger.handlers:
             handler.close()
             experiment_logger.removeHandler(handler)
-    
+
     else:
         logger = logging.getLogger()
         for handler in logger.handlers:
             handler.close()
             logger.removeHandler(handler)
-        
